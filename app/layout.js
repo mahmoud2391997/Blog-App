6a90e346215295e0be7803697f698363e91fb106
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-09-15 22:06:36 +0300

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./navbar";
+import Navbar from "./NavBar";
 import Footer from "./footer";
 
 const inter = Inter({ subsets: ["latin"] });
